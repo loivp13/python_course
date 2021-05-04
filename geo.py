12e8d1835745961f4844180528b8ee81273f3701
Loi <loivp13@gmail.com>
2021-05-04 13:35:32 -0700

    add automatically generated of markers

--- geo.py
+++ geo.py
@@ -29,16 +29,30 @@ class Geopoint(Marker) :
         return cls(latitude = uniform(-90, 90), longitude = uniform(-180, 180))
 
 
-latitude = 40.4
-longitude = -3.7
+
 
 # folium instance
-mymap = Map(location = [latitude, longitude])
+mymap = Map(location = [40, 2])
+
 
 # geopoint instance
-geopoint = Geopoint(latitude=latitude, longitude= longitude)
-popup = Popup(str(geopoint.get_weather()))
-popup.add_to(geopoint)
-geopoint.add_to(mymap)
+locations = [[41,-1],[40,2],[39,5]]
+for loc in locations:
+    geopoint = Geopoint(latitude=loc[0], longitude= loc[1])
+    forecast = geopoint.get_weather()
+    popup_content = f"""
+    {forecast[0][0][-8:-6]}h: {round(forecast[0][1])}°F <img src='https://clipground.com/images/weather-icon-transparent-png-5.jpg' width = 35>
+    <hr style='margin:1px'>
+    {forecast[1][0][-8:-6]}h:{round(forecast[1][1])}°F <img src='https://clipground.com/images/weather-icon-transparent-png-5.jpg' width = 35>
+    <hr style='margin:1px'>
+    {forecast[2][0][-8:-6]}h:{round(forecast[2][1])}°F <img src='https://clipground.com/images/weather-icon-transparent-png-5.jpg' width = 35>
+    <hr style='margin:1px'>
+    {forecast[3][0][-8:-6]}h:{round(forecast[3][1])}°F <img src='https://clipground.com/images/weather-icon-transparent-png-5.jpg' width = 35>
+    <hr style='margin:1px'>
+    """
+
+    popup = Popup(popup_content,max_width=400)
+    popup.add_to(geopoint)
+    geopoint.add_to(mymap)
 
 mymap.save('map.html')
